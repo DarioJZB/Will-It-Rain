28c16b6180f282fffb3a588e69fab1dfb78078ec
DarioJZB <dariojzb87@gmail.com>
2024-09-23 01:20:56 -0400

    single city search code working for current and next 5 days

--- server/src/service/historyService.ts
+++ server/src/service/historyService.ts
@@ -1,9 +1,10 @@
 // TODO: Define a City class with name and id properties
 
+
 // TODO: Complete the HistoryService class
 class HistoryService {
   // TODO: Define a read method that reads from the searchHistory.json file
-  // private async read() {}
+  //private async read() {}
   // TODO: Define a write method that writes the updated cities array to the searchHistory.json file
   // private async write(cities: City[]) {}
   // TODO: Define a getCities method that reads the cities from the searchHistory.json file and returns them as an array of City objects
